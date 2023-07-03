870e01e675ca6a9a34856184ce0dc1302c6b7df3
vishnu biju <vishnubiju.in@gmail.com>
2023-07-04 01:19:30 +0530

    feat: success page

---
+++ client/src/components/Success.tsx
@@ -0,0 +1,50 @@
+
+import { useParams } from "react-router-dom";
+import { Typography, Paper, Grid } from "@mui/material";
+
+const SuccessPage = () => {
+  const { firstName, lastName, email, contactMode } = useParams();
+
+  return (
+    <Grid
+      container
+      style={{
+        height: "100vh",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Grid item xs={12} md={6} sm={6} style={{ width: "100%" }}>
+        <Paper
+          sx={{
+            paddingX: 4,
+            paddingY: 6,
+            borderRadius: 4,
+            backgroundColor: "#3A244A",
+            color: "#FFFFFF",
+          }}
+          style={{ margin: "0 auto", width: "50%", padding: "28px" }}
+        >
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            Verification Complete
+          </Typography>
+          <Typography variant="subtitle1" sx={{ marginTop: 2 }}>
+            Congratulations! Your registration/login is successful.
+          </Typography>
+          <Typography variant="body1" sx={{ marginTop: 2 }}>
+            <span style={{ color: "#D72638" }}>Name:</span> {firstName} {lastName}
+          </Typography>
+          <Typography variant="body1">
+            <span style={{ color: "#D72638" }}>Email:</span> {email}
+          </Typography>
+          <Typography variant="body1">
+            <span style={{ color: "#D72638" }}>Contact Mode:</span> {contactMode}
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SuccessPage;
